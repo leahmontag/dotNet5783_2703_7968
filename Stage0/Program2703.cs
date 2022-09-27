4aca3763adcb96755dfc200af14842b9caab61c2
leahMontag <mirimontag@gmail.com>
2022-09-28 00:14:11 +0300

    Initial code + welcome functions

---
+++ Stage0/Program2703.cs
@@ -0,0 +1,18 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        welcome2703();
+        welcome7968();
+        Console.ReadKey();
+    }
+
+    static partial void welcome7968();
+    private static void welcome2703()
+    {
+        Console.Write("Enter your name: ");
+        string userName;
+        userName = Console.ReadLine();
+        Console.Write("{0},welcome to my first console application", userName);
+    }
+}
