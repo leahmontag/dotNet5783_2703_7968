1270e9e4358226d72f28ab9d51e37d99adf81d2b
leahMontag <mirimontag@gmail.com>
2022-12-15 18:37:07 +0200

    singleton stage 4

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -1,11 +1,24 @@
 ﻿using DalApi;
-using DO;
 namespace Dal;
 
-sealed public class DalList : IDal
+sealed internal class DalList : IDal
 {
+    private static object syncRoot = new object();
+    public static IDal Instance { get; }
+    private DalList() { }
+    static DalList()
+    {
+        if (Instance == null)
+        {
+            lock (syncRoot)
+            {
+                if (Instance == null)
+                    Instance = new DalList();
+            }
+        }
+    }
     public IProduct Product => new DalProduct();
     public IOrder Order => new DalOrder();
     public IOrderItem OrderItem => new DalOrderItem();
-}
 
+}
