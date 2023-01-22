376bea088fc8f00814ecb9314a002f1731766529
EfratGrinboom <shifraefrat@gmail.com>
2023-01-22 18:49:54 +0200

    xml files

--- DalFacade/Class1.cs
+++ DalFacade/Class1.cs
@@ -1,7 +1,20 @@
-﻿namespace DalFacade
-{
-    public class Class1
-    {
+﻿using System.Xml.Linq;
 
-    }
+namespace DalFacade;
+
+public class XmlStudents
+{
+   // public class XmlStudents
+
+ //   {
+
+        XElement root;
+
+        string path = @"..\baba\baba.xml";
+
+        public XmlStudents() => root = new XElement("students");
+
+//    }
+
+
 }
